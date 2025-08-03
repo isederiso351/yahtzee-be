a9de7093d696d25902bb28b4a614e9708865717a
isederiso351 <alessandrobruno.0220@gmail.com>
2025-08-03 11:29:22 +0200

    First commit

---
+++ src/main/java/org/example/yahtzee_be/YahtzeeBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.yahtzee_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YahtzeeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YahtzeeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/yahtzee_be/YahtzeeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.yahtzee_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YahtzeeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
