ddc1c25ac1165392acd9868168f25baee2f79d6e
isederiso351 <alessandrobruno.0220@gmail.com>
2025-08-06 15:20:30 +0200

    feature(keycloack): keycloack implemented

---
+++ src/main/java/org/example/yahtzee_be/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package org.example.yahtzee_be.config;
+
+import org.example.yahtzee_be.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, UserService userService) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        //.requestMatchers("/public/**").permitAll() // endpoint pubblici
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .oidcUserService(request -> handleOidcUser(userService, request))
+                        )
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+
+        return http.build();
+    }
+
+    private OidcUser handleOidcUser(UserService userService, OidcUserRequest userRequest) {
+        OidcUserService delegate = new OidcUserService();
+        OidcUser oidcUser = delegate.loadUser(userRequest);
+
+        String email = oidcUser.getEmail();
+
+        userService.syncUser(email);
+
+        return oidcUser;
+    }
+}
