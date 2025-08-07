1659119825cfedac2bb62b18f08e9ade2b895514
isederiso351 <alessandrobruno.0220@gmail.com>
2025-08-07 23:31:16 +0200

    feature(websocket): websocket implementation

---
+++ src/main/java/org/example/yahtzee_be/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package org.example.yahtzee_be.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // Prefisso dei messaggi destinati ai client
+        config.enableSimpleBroker("/topic");
+        // Prefisso per i messaggi inviati dal client al server
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+}
+
